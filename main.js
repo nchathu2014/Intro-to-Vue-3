f8a511ae2a46682f38de2218388a97eda69a5339
Chathuranga <nchathu2014@gmail.com>
2021-11-16 07:38:50 +0530

    Binding attributes

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            product: 'Socks'
-        }
-    }
-})
+  data() {
+    return {
+      product: "Socks",
+      image: "./assets/images/socks_blue.jpg",
+      img_alt: "Image Alt",
+      url: "http://images/1.jpg",
+    };
+  },
+});
